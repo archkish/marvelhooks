1face0583cd86499e2cb3ef48b747ffa1f64243c
Artur Radchenko <kisharchi@gmail.com>
2025-02-06 22:41:04 +0100

    add form for searching a character

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -7,7 +7,13 @@ import Spinner from "../spiner/Spinner";
 const Page404 = lazy(() => import("../pages/404"));
 const MainPage = lazy(() => import("../pages/MainPage"));
 const ComicsPage = lazy(() => import("../pages/ComicsPage"));
-const SingleComicPage = lazy(() => import("../pages/SingleComicPage"));
+const SingleComicLayout = lazy(() =>
+  import("../pages/singleComicLayout/SingleComicLayout")
+);
+const SingleCharacterLayout = lazy(() =>
+  import("../pages/singleCharacterLayout/SingleCharacterLayout")
+);
+const SinglePage = lazy(() => import("../pages/SinglePage"));
 
 const App = () => {
   return (
@@ -15,12 +21,21 @@ const App = () => {
       <div className="app">
         <AppHeader />
         <main>
-          <Suspense fallback={<Spinner/>}>
+          <Suspense fallback={<Spinner />}>
             <Routes>
-              <Route path="/" element={<MainPage />} />
-              <Route path="/comics" element={<ComicsPage />} />
-              <Route path="/comics/:comicId" element={<SingleComicPage />} />
-              <Route path="*" element={<Page404 />} />
+              <Route 
+                    path="/" 
+                    element={<MainPage />}/>
+              <Route 
+                    path="/comics" 
+                    element={<ComicsPage />}/>
+              <Route 
+                    path="/comics/:id" 
+                    element={<SinglePage Component={SingleComicLayout} dataType="comic" />}/>
+              <Route 
+                    path="/characters/:id" 
+                    element={<SinglePage Component={SingleCharacterLayout} dataType="character" />}/>
+              <Route path="*" element={<Page404 />}/>
             </Routes>
           </Suspense>
         </main>
